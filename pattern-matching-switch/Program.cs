d751d5d469a3f5a12c42067b95ed58481bde4f61
Pandiyan <pmurugan@burning-glass.com>
2017-03-13 19:11:13 +0530

    Initial commit

---
+++ pattern-matching-switch/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int experience = 5;
+
+        // Pattern matching in switch cases
+        switch (experience)
+        {
+            case var exp when (experience > 0 & experience < 4):
+                Console.Write("Junior stage with {0} years of experience level", exp);
+                break;
+
+            case var exp when (experience > 10):
+                Console.Write("Super senior stage with {0} years of experience level", exp);
+                break;
+
+            case var exp when (new List<int>() { 4, 5, 6, 7 }).Contains(experience):
+                Console.Write("Mid level stage with {0} years of experience level", exp);
+                break;
+
+            default:
+                Console.Write("code hits the default case");
+                break;
+        }
+    }
+}
